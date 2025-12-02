a98e2219e6582cef1daa74f136106302d1cbcf65
yaider <yaideralejo2@gmail.com>
2025-12-01 19:49:37 -0500

    Cambios en conts e index

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'CV Yaider Becerra';
+export const SITE_DESCRIPTION = 'CV para conseguir chamba';
